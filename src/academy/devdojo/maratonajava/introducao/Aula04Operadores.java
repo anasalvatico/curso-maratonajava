a41e03efaa23d55a0b29f859a35eb35bd51b3370
anasalvatico <anaclarasalvatico@gmail.com>
2025-08-31 15:55:27 -0300

    Adiciona o operador resto

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -2,9 +2,14 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
+        // + - / *
         int numero01 = 10;
         double numero02 = 20;
         double resultado = numero01 / numero02;
         System.out.println(resultado);
+
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
     }
 }
