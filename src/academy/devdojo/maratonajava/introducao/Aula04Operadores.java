1cb825aa1e4de09d3b513b54e0d887d36e6e06b0
anasalvatico <anaclarasalvatico@gmail.com>
2025-08-31 19:13:26 -0300

    Adiciona os operadores de atribuição

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -40,6 +40,24 @@ public class Aula04Operadores {
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > 5000 || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
 
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        //++ --
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        System.out.println(contador);
+
+        int contador2 = 0;
+        System.out.println(++contador2);
     }
 }
 
