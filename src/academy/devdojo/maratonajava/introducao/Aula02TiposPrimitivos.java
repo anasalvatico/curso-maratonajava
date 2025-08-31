ad553ac648d20908b9ae71bec9af659f0f5c7cd1
anasalvatico <anaclarasalvatico@gmail.com>
2025-08-31 13:41:43 -0300

    Adiciona a string nome no código

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,17 +4,19 @@ public class Aula02TiposPrimitivos  {
     public static void main(String[] args) {
         int idade = 10;
         long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.0F;
         byte idadeByte = 10;
         short idadeShort = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u0041';
+        String nome = "Ana";
 
         System.out.println("A idade é " +idade+" anos");
         System.out.println(falso);
         System.out.println("char "+caractere);
+        System.out.println(nome);
 
     }
 }
