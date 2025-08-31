1a180bcb502f3f42d600270ce3d12abe049ae6d1
anasalvatico <anaclarasalvatico@gmail.com>
2025-08-31 15:46:03 -0300

    Adiciona a classe Aula04Operadores

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero01 = 10;
+        double numero02 = 20;
+        double resultado = numero01 / numero02;
+        System.out.println(resultado);
+    }
+}
