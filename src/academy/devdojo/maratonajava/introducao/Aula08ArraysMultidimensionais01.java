b5cf2e641d791b7d5b475f960b06f51a9dcbef6d
anasalvatico <anaclarasalvatico@gmail.com>
2025-09-20 19:30:51 -0300

    Adiciona a classe Aula08ArraysMultidimensionais01

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 29;
+        dias[0][2] = 28;
+
+        dias[1][0] = 20;
+        dias[1][1] = 19;
+        dias[1][2] = 18;
+
+        dias[2][0] = 7;
+        dias[2][1] = 6;
+        dias[2][2] = 5;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[0].length ; j++) {
+                System.out.println(dias[i][j]);
+            }
+
+        }
+
+    }
+
+}
