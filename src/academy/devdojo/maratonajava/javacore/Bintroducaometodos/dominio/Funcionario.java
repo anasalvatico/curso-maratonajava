613123ddf3f21d87272eec81986c135b53856790
anasalvatico <anaclarasalvatico@gmail.com>
2026-01-06 19:35:20 -0300

    Aula modificadores de acesso 2

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -4,6 +4,7 @@ public class Funcionario {
    public String nome;
    public int idade;
    public double[] salarios;
+   private double media;
 
    public void imprime(){
        System.out.println(this.nome);
@@ -17,11 +18,38 @@ public class Funcionario {
    }
 
    public void imprimeMediaSalarios(){
-       double media = 0;
        for(double salario: salarios){
            media += salario;
        }
        media /= salarios.length;
        System.out.println("Média salarial "+media);
    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public double getMedia() {
+        return media;
+    }
 }
