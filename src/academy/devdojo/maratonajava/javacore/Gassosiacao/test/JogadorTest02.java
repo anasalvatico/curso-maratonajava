9243fd232820509c005c1c18ab1756c51061e820
anasalvatico <anaclarasalvatico@gmail.com>
2026-02-05 01:50:54 -0300

    Aula associação pt 2

---
+++ src/academy/devdojo/maratonajava/javacore/Gassosiacao/test/JogadorTest02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Gassosiacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassosiacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassosiacao.dominio.Time;
+
+public class JogadorTest02 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Time time = new Time("Seleção Brasileira");
+
+        jogador1.setTime(time);
+
+        jogador1.imprime();
+    }
+}
